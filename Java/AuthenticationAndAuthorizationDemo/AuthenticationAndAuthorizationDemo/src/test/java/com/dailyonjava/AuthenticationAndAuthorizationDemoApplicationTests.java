1486dc777d553f9dab94327e12ce06376f89fdb4
patelajay005 <patelajay@live.in>
2025-09-16 09:01:55 +0530

    First commit : added Java and AI folder directory

---
+++ Java/AuthenticationAndAuthorizationDemo/AuthenticationAndAuthorizationDemo/src/test/java/com/dailyonjava/AuthenticationAndAuthorizationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyonjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAndAuthorizationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Java/AuthenticationAndAuthorizationDemo/src/main/java/com/dailyonjava/AuthenticationAndAuthorizationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dailyonjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationAndAuthorizationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationAndAuthorizationDemoApplication.class, args);
+	}
+
+}
